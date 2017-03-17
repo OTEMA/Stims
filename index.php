0b772c8fb52f3bebaa369ed67efa88f44d3737f4
Tosby <o2jose43@gmail.com>
2017-03-17 14:22:40 +0300

    Begun the design of the login pages

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ A breech of this may lead to prosecution.
         <div id="container">
             <img src="resources/images/msu_banner2.png" class="image-responsive header-image"/>
             <h1 class="text-center text-primary header-text">Welcome to the results portal!</h1>
-            <div class="navbar navbar-inverse navbar-static-top my-navbar" data-spy="affix" data-offset-top="197" style="background-color: #006633; width: 100%; margin-top: -3.2px; padding: 0;">
+            <div class="navbar navbar-inverse navbar-static-top" data-spy="affix" data-offset-top="197" style="width: 100%; margin-top: -3.2px; padding: 0;">
                 <div class="container">
                     <a href="http://www.maseno.ac.ke" class="navbar-brand">Online Results </a>
                     <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
@@ -45,31 +45,17 @@ A breech of this may lead to prosecution.
             </div>
             <div id="body">
                 <div class="rows">
-                    <div class="col-md-2">
-                        <p><b>Welcome to the exam results portal</b></p>
-                        <p>This system allows you to view your results and fee balance.
-                            <br> Lecturers and teachers can also upload the results easily and the accounts department can show fees transparency.</p>
+                    <div class="col-sm-6 col-md-4">
+                        <p><b>Welcome to the student Information management system</b></p>
+                        <p>This system allows you to view your results and other informations.
+                            <br> The administrator can also upload and manage student details easily.</p>
                         <p><b class="bg-note">New users:</b> Please click <a href="pages/register_user.php">Register Now</a> and enter the requested information. Upon successful registration, you will be sent an email with instructions and your log-in information.</p>
                         <p><b class="bg-note">Existing users:</b> Do not register again. Simply enter your username and password and click the Investigator log in button. If you have forgotten your details please click <a href="password_reset.php">Forgot Password.</a></p>
                     </div>
-                    <div class="col-md-8 well">
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label for="uname">User name</label>
-                                <input type="text" class="form-control" id="uname"autocomplete="on" placeholder="Enter your user name" required="required">
-                            </div>
-                            <div class="form-group">
-                                <label for="pwd">Password</label>
-                                <input type="password" id="pwd" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" class="form-control btn-primary" value="Login">
-                            </div>
-                        </form>
+                    <div class="col-sm-6 col-md-8 well">
+
                     </div>
-                    <div class="col-md-2"></div>
                 </div>
             </div>
-        </div>
     </body>
 </html>
