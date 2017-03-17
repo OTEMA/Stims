159c8a9b245e325a9a973895c67ec0f46e9c7f1c
Tosby <o2jose43@gmail.com>
2017-03-17 11:23:30 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+This application is property of Otema and is licenced under otema ©2017
+No part of it whatsoever shall be shared or used unless under written permission by Otema
+A breech of this may lead to prosecution.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="resources/css/Style.css" rel="stylesheet" type="text/css"/>
+        <link href="resources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="resources/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="resources/js/bootstrap.js" type="text/javascript"></script>
+        <script src="resources/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="resources/js/jquery/dist/jquery.js" type="text/javascript"></script>
+        <script src="resources/js/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
