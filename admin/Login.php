2571f7ed22d45920780ccd2476227c4f04f049c4
Tosby <o2jose43@gmail.com>
2017-03-18 17:53:54 +0300

    finished the design of the login pages

--- admin/Login.php
+++ admin/Login.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-This application is property of Otema and is licenced under otema ©2017
-No part of it whatsoever shall be shared or used unless under written permission by Otema
-A breech of this may lead to prosecution.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -20,8 +15,7 @@ A breech of this may lead to prosecution.
         <link href="../resources/css/Style.css" rel="stylesheet" type="text/css"/>
         <link href="../resources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <!-- js -->
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="../resources/js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <!-- //js -->
         <link href="../resources/css/flexslider.css" rel="stylesheet" type="text/css"/>
         <!-- font-awesome-icons -->
@@ -33,13 +27,13 @@ A breech of this may lead to prosecution.
             (function (i, s, o, g, r, a, m) {
                 i['GoogleAnalyticsObject'] = r;
                 i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
+                    (i[r].q = i[r].q || []).push(arguments);
                 }, i[r].l = 1 * new Date();
                 a = s.createElement(o),
                         m = s.getElementsByTagName(o)[0];
                 a.async = 1;
                 a.src = g;
-                m.parentNode.insertBefore(a, m)
+                m.parentNode.insertBefore(a, m);
             })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
             ga('create', 'UA-40923671-1', 'lakehub.co.ke');
