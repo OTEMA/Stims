42907671705276f089a5823fd0eec4103fed13ac
Tosby <o2jose43@gmail.com>
2017-03-18 20:28:35 +0300

    Added a js for copyrights

--- resources/js/Copyright.js
+++ resources/js/Copyright.js
@@ -1,5 +1,27 @@
 
     var year = new Date().getFullYear();
     document.getElementById("year").innerHTML = year;
+    
+    window.slideleft = function($elem) {
+    var top = parseInt($elem.css("top"));
+    var temp = -1 * $('#slideleft  > div').height();
+    if(top < temp) {
+        top = $('#slideleft ').height();
+        $elem.css("top", top);
+    }
+    $elem.animate({ top: (parseInt(top)-60) }, 600, function () {
+      window.slideleft ($(this));
+    });
+};
+
+
+$(document).ready(function() {
+    var i = 0;
+    $("#slideleft  > div").each(function () {
+          $(this).css("top", i);
+          i += 60;
+          window.slideleft ($(this));
+    });
+});
 
 
