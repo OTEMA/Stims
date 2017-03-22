4f86ee428787549d68eb626dead62f8ebdc83ba5
Tosby <o2jose43@gmail.com>
2017-03-22 17:58:49 +0300

    Added the controller files

---
+++ Controller/Registration_controller.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+ * This application is property of Otema and is licenced under otema ©2017
+ * No part of it whatsoever shall be shared or used unless under written permission by Otema
+ * A breech of this may lead to prosecution.
+ */
+
