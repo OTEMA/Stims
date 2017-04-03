da6e53a7ce5f158f7070a9dcdcf15f34ae48e15b
Tosby <o2jose43@gmail.com>
2017-04-03 16:48:59 +0300

    Finished writing the logout functions

--- Controller/logout.php
+++ Controller/logout.php
@@ -9,16 +9,25 @@ session_destroy();
 <head>
   <meta charset="UTF-8">
   <title>Error</title>
-  <?php include 'css/css.html'; ?>
+  <link href="../resources/css/Appearance.css" rel="stylesheet" type="text/css"/>
+  <link href="../resources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+  <link href="../resources/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<!--  JavaScript files-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
     <div class="form">
-          <h1>Thanks for stopping by</h1>
+        <h1 style="color: #FFFFFF;">Thanks for stopping by</h1>
               
-          <p><?= 'You have been logged out!'; ?></p>
+        <p style="color: #1ab188;"><?= 'You have been logged out!'; ?></p>
           
-          <a href="index.php"><button class="button button-block"/>Home</button></a>
+          <a href="../index.php"><button class="button button-block"/>Home</button></a>
 
     </div>
 </body>
