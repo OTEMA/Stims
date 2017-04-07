fd610df3f2f01b5c7288258d4564241794b0a89a
Tosby <o2jose43@gmail.com>
2017-04-07 21:05:49 +0300

    Finished writing the student registration logic

--- admin/Register_student.php
+++ admin/Register_student.php
@@ -13,9 +13,52 @@ $SC = $mysqli->escape_string($_POST['SC']);
 $Dept = $mysqli->escape_string($_POST['Dept']);
 $program = $mysqli->escape_string($_POST['program']);
 $Tel = $mysqli->escape_string($_POST['Tel']);
-$email = $mysqli->escape_string($_POST['email']);
+$email = $mysqli->escape_string($_POST['Email']);
 $password = $mysqli->escape_string(password_hash($_POST['IdNo'], PASSWORD_BCRYPT));
 $hash = $mysqli->escape_string( md5( rand(0,1000) ) );
       
+// Check if Admission number and Id Number already exists
+$result = $mysqli->query("SELECT * FROM students WHERE AdmNo='$AdmNo' OR IdNo='$IdNo'") or die($mysqli->error);
 
+
+if ( $result->num_rows > 0 ) {
+    
+    $_SESSION['message'] = 'Student already exists!';
+    header("location: ../Controller/error.php");
+    
+}
+else{
+    $sql = "INSERT INTO admins (Fname, Mname, Lname, AdmNo, DOB, DOA, IdNo, Schoo, Dept, Prog_name, Tel, Email, Password, Hash) VALUES ('$Fname','$Mname','$Lname','$AdmNo','$date','$date1','$IdNo','$SC','$Dept','$program', '$Tel', '$email','$password', '$hash')";
+}
+if ( $mysqli->query($sql) ){
+
+        $_SESSION['active'] = 0; //0 until user activates their account with verify.php
+        $_SESSION['logged_in'] = true; // So we know the user has logged in
+        $_SESSION['message'] =
+                
+                 "Confirmation link has been sent to $email, please verify
+                 your account by clicking on the link in the message!";
+
+        // Send registration confirmation link (verify.php)
+        $to      = $email;
+        $subject = 'Account Verification ( otelmaltd@gmail.com )';
+        $message_body = '
+        Hello '.$first_name.',
+
+        Thank you for signing up!
+
+        Please click this link to activate your account:
+
+        http://localhost/Stims/student/verify.php?email='.$email.'&hash='.$hash;  
+
+        mail( $to, $subject, $message_body );
+
+        header("location: Add_register.php"); 
+
+    }
+
+    else {
+        $_SESSION['message'] = 'Registration failed!';
+        header("location: ../Controller/error.php");
+    }
 
