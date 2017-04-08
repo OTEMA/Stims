4efcda8812a88d1205a7604be45bad0770773577
Tosby <o2jose43@gmail.com>
2017-04-08 17:34:00 +0300

    Edited the Success file to add  a temporary link for reseting password

---
+++ admin/Register_student.php
@@ -0,0 +1,62 @@
+<?php
+require '../Controller/db.php';
+
+// Protection against SQL injections
+$Fname = $mysqli->escape_string($_POST['Fname']);
+$Mname = $mysqli->escape_string($_POST['Mname']);
+$Lname = $mysqli->escape_string($_POST['Lname']);
+$AdmNo = $mysqli->escape_string($_POST['AdmNo']);
+$date = $mysqli->escape_string($_POST['date']);
+$date1 = $mysqli->escape_string($_POST['date1']);
+$IdNo = $mysqli->escape_string($_POST['IdNo']);
+$SC = $mysqli->escape_string($_POST['SC']);
+$Dept = $mysqli->escape_string($_POST['Dept']);
+$program = $mysqli->escape_string($_POST['program']);
+$Tel = $mysqli->escape_string($_POST['Tel']);
+$email = $mysqli->escape_string($_POST['Email']);
+$password = $mysqli->escape_string(password_hash($_POST['IdNo'], PASSWORD_BCRYPT));
+$hash = $mysqli->escape_string( md5( rand(0,1000) ) );
+      
+// Check if Admission number and Id Number already exists
+$result = $mysqli->query("SELECT * FROM students WHERE AdmNo='$AdmNo' OR IdNo='$IdNo'") or die($mysqli->error);
+
+
+if ( $result->num_rows > 0 ) {
+    
+    $_SESSION['message'] = 'Student already exists!';
+    header("location: ../Controller/error.php");
+    
+}
+else{
+    $sql = "INSERT INTO admins (Fname, Mname, Lname, AdmNo, DOB, DOA, IdNo, Schoo, Dept, Prog_name, Tel, Email, Password, Hash) VALUES ('$Fname','$Mname','$Lname','$AdmNo','$date','$date1','$IdNo','$SC','$Dept','$program', '$Tel', '$email','$password', '$hash')";
+}
+if ( $mysqli->query($sql) ){
+
+        $_SESSION['active'] = 0; //0 until user activates their account with verify.php
+        $_SESSION['message'] =
+                
+                 "User Added successully!";
+
+        // Send registration confirmation link (verify.php)
+        $to      = $email;
+        $subject = 'Account Verification ( otelmaltd@gmail.com )';
+        $message_body = '
+        Hello '.$first_name.',
+
+        Thank you for signing up!
+
+        Please click this link to activate your account:
+
+        http://localhost/Stims/student/verify.php?email='.$email.'&hash='.$hash;  
+
+        mail( $to, $subject, $message_body );
+
+        header("location: Add_register.php"); 
+
+    }
+
+    else {
+        $_SESSION['message'] = 'Registration failed!';
+        header("location: ../Controller/error.php");
+    }
+
