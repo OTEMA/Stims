b8a53658b7d371358aae4d7d5c5ae58a93d1175a
Tosby <o2jose43@gmail.com>
2017-04-10 20:50:39 +0300

    Added a test database file

--- Controller/stims.sql
+++ Controller/stims.sql
@@ -1,14 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2017 at 02:58 PM
--- Server version: 5.7.11
--- PHP Version: 7.0.4
+-- Generation Time: Apr 10, 2017 at 05:46 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+03:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -35,8 +35,17 @@ CREATE TABLE `admins` (
   `Tel` varchar(12) NOT NULL,
   `Email` varchar(60) NOT NULL,
   `Password` varchar(60) NOT NULL,
-  `Time` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP
+  `Hash` text NOT NULL,
+  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `active` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`Fname`, `Mname`, `Lname`, `StaffNo`, `IdNo`, `Tel`, `Email`, `Password`, `Hash`, `Time`, `active`) VALUES
+('Test', 'Test', 'Test', 'Test', 28789722, 'Test', 'test@test.com', '$2y$10$jO78.tvtxBUhEAskgvD29OoUWl7WexYkiuFcJDiSirDzHqG8cpssu', '07cdfd23373b17c6b337251c22b7ea57', '2017-04-03 10:14:57', 0);
 
 -- --------------------------------------------------------
 
@@ -45,11 +54,30 @@ CREATE TABLE `admins` (
 --
 
 CREATE TABLE `courses` (
-  `Course_Id` int(11) NOT NULL,
-  `AdmNo` varchar(30) NOT NULL,
+  `Prog_Id` int(11) NOT NULL,
   `Course_Name` varchar(100) NOT NULL,
   `Course_Code` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `Department_id` int(11) NOT NULL,
+  `Dept_Name` varchar(255) NOT NULL,
+  `School_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`Department_id`, `Dept_Name`, `School_id`) VALUES
+(1, 'IT', 1),
+(2, 'SNE', 2);
 
 -- --------------------------------------------------------
 
@@ -69,6 +97,39 @@ CREATE TABLE `fees` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `Path` varchar(255) NOT NULL,
+  `Name` varchar(65) NOT NULL,
+  `AdmNo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programs`
+--
+
+CREATE TABLE `programs` (
+  `Prog_name` varchar(255) NOT NULL,
+  `Prog_Id` int(11) NOT NULL,
+  `Department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `programs`
+--
+
+INSERT INTO `programs` (`Prog_name`, `Prog_Id`, `Department_id`) VALUES
+('BscIT', 1, 1),
+('BedSne', 2, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `results`
 --
 
@@ -79,6 +140,25 @@ CREATE TABLE `results` (
   `AdmNo` varchar(30) NOT NULL,
   `Results` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schools`
+--
+
+CREATE TABLE `schools` (
+  `School_name` varchar(255) NOT NULL,
+  `School_Id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schools`
+--
+
+INSERT INTO `schools` (`School_name`, `School_Id`) VALUES
+('Computing and Informatics', 1),
+('Education', 2);
 
 -- --------------------------------------------------------
 
@@ -96,10 +176,12 @@ CREATE TABLE `students` (
   `IdNo` varchar(12) NOT NULL,
   `School` varchar(255) NOT NULL,
   `Dept` varchar(255) NOT NULL,
-  `Course` varchar(255) NOT NULL,
+  `Prog_Id` int(11) NOT NULL,
   `Tel` int(12) NOT NULL,
   `Email` varchar(60) NOT NULL,
-  `Password` varchar(60) NOT NULL
+  `Password` varchar(60) NOT NULL,
+  `Hash` varchar(255) NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -118,7 +200,16 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `courses`
   ADD PRIMARY KEY (`Course_Code`),
-  ADD KEY `AdmNo` (`AdmNo`);
+  ADD KEY `AdmNo` (`Prog_Id`),
+  ADD KEY `AdmNo_2` (`Prog_Id`),
+  ADD KEY `Prog_Id` (`Prog_Id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`Department_id`),
+  ADD KEY `School_id` (`School_id`);
 
 --
 -- Indexes for table `fees`
@@ -126,6 +217,21 @@ ALTER TABLE `courses`
 ALTER TABLE `fees`
   ADD PRIMARY KEY (`Id`),
   ADD KEY `AdmNo` (`AdmNo`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`AdmNo`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `AdmNo` (`AdmNo`);
+
+--
+-- Indexes for table `programs`
+--
+ALTER TABLE `programs`
+  ADD PRIMARY KEY (`Prog_Id`),
+  ADD KEY `Department_id` (`Department_id`);
 
 --
 -- Indexes for table `results`
@@ -136,30 +242,57 @@ ALTER TABLE `results`
   ADD KEY `AdmNo` (`AdmNo`);
 
 --
+-- Indexes for table `schools`
+--
+ALTER TABLE `schools`
+  ADD PRIMARY KEY (`School_Id`);
+
+--
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`AdmNo`),
-  ADD UNIQUE KEY `IdNo` (`IdNo`);
+  ADD UNIQUE KEY `IdNo` (`IdNo`),
+  ADD KEY `Prog_Id` (`Prog_Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `Department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `programs`
+--
+ALTER TABLE `programs`
+  MODIFY `Prog_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
 -- AUTO_INCREMENT for table `results`
 --
 ALTER TABLE `results`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `schools`
+--
+ALTER TABLE `schools`
+  MODIFY `School_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `courses`
+-- Constraints for table `department`
 --
-ALTER TABLE `courses`
-  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`AdmNo`) REFERENCES `students` (`AdmNo`);
+ALTER TABLE `department`
+  ADD CONSTRAINT `department_ibfk_1` FOREIGN KEY (`School_id`) REFERENCES `schools` (`School_Id`);
 
 --
 -- Constraints for table `fees`
@@ -168,11 +301,24 @@ ALTER TABLE `fees`
   ADD CONSTRAINT `fees_ibfk_1` FOREIGN KEY (`AdmNo`) REFERENCES `students` (`AdmNo`);
 
 --
+-- Constraints for table `programs`
+--
+ALTER TABLE `programs`
+  ADD CONSTRAINT `programs_ibfk_1` FOREIGN KEY (`Department_id`) REFERENCES `department` (`Department_id`);
+
+--
 -- Constraints for table `results`
 --
 ALTER TABLE `results`
   ADD CONSTRAINT `results_ibfk_1` FOREIGN KEY (`Course_code`) REFERENCES `courses` (`Course_Code`),
   ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`AdmNo`) REFERENCES `students` (`AdmNo`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`AdmNo`) REFERENCES `images` (`AdmNo`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`Prog_Id`) REFERENCES `programs` (`Prog_Id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
