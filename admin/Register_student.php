6b09a36d59329f9be8ef1a89d7ba77baf1bfdad4
Tosby <o2jose43@gmail.com>
2017-04-11 19:45:51 +0300

    Removed errors on the student registration controller file

--- admin/Register_student.php
+++ admin/Register_student.php
@@ -1,7 +1,8 @@
 <?php
-require '../Controller/db.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+require '../Controller/db.php';
+session_start();
+
 $Fname = $mysqli->escape_string($_POST['Fname']);
 $Mname = $mysqli->escape_string($_POST['Mname']);
 $Lname = $mysqli->escape_string($_POST['Lname']);
@@ -15,48 +16,42 @@ $program = $mysqli->escape_string($_POST['program']);
 $Tel = $mysqli->escape_string($_POST['Tel']);
 $email = $mysqli->escape_string($_POST['Email']);
 $password = $mysqli->escape_string(password_hash($_POST['IdNo'], PASSWORD_BCRYPT));
-$hash = $mysqli->escape_string( md5( rand(0,1000) ) );
-      
+$hash = $mysqli->escape_string(md5(rand(0, 1000)));
+
 // Check if Admission number and Id Number already exists
 $result = $mysqli->query("SELECT * FROM students WHERE AdmNo='$AdmNo' OR IdNo='$IdNo'") or die($mysqli->error);
 
 
-if ( $result->num_rows > 0 ) {
-    
+if ($result->num_rows > 0) {
+
     $_SESSION['message'] = 'Student already exists!';
     header("location: ../Controller/error.php");
-    
-}
-else{
-    $sql = "INSERT INTO students (Fname, Mname, Lname, AdmNo, DOB, DOA, IdNo, School, Dept, Prog_name, Tel, Email, Password, Hash) VALUES ('$Fname','$Mname','$Lname','$AdmNo','$date','$date1','$IdNo','$SC','$Dept','$program', '$Tel', '$email','$password', '$hash')";
-
-if ( $mysqli->query($sql) ){
+} else {
+    $sql = "INSERT INTO students (Fname, Mname, Lname, AdmNo, DOB, DOA, IdNo, School, Dept, Prog_Id, Tel, Email, Password, Hash)"
+            . " VALUES "
+            . "('$Fname','$Mname','$Lname','$AdmNo','$date','$date1','$IdNo','$SC','$Dept','$program', '$Tel', '$email','$password', '$hash')";
+    if ($mysqli->query($sql)) {
 
         $_SESSION['active'] = 0; //0 until user activates their account with verify.php
         $_SESSION['message'] = "User Added successully!";
 
         // Send registration confirmation link (verify.php)
-        $to      = $email;
+        $to = $email;
         $subject = 'Account Verification ( otelmaltd@gmail.com )';
         $message_body = '
-        Hello '.$Fname.',
+        Hello ' . $Fname . ',
 
-        You have been registered for '.$program.',
+        You have been registered for' . $program . '!
 
         Please click this link to activate your account:
 
-        http://localhost/Stims/student/verify.php?email='.$email.'&hash='.$hash;  
+        http://localhost/StudentInformationManagementSystem/student/verify.php?email=' . $email . '&hash=' . $hash;
 
-        mail( $to, $subject, $message_body );
+        mail($to, $subject, $message_body);
 
-        header("location: Add_register.php"); 
-
-    }
-
-    else {
+        header("location: Add_register.php");
+    } else {
         $_SESSION['message'] = 'Registration failed!';
         header("location: ../Controller/error.php");
     }
-
-}
 }
