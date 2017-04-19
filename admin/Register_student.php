e75fb76e62c5f48b8ee03dc1786a3c11d65e2123
Tosby <o2jose43@gmail.com>
2017-04-19 11:42:15 +0300

    Changed the default student password to be the studen'ts admission number inorder to maintain uniqueness, of the logged in users and to carter for students who may not have Id numbers

--- admin/Register_student.php
+++ admin/Register_student.php
@@ -15,7 +15,7 @@ $Dept = $mysqli->escape_string($_POST['Dept']);
 $program = $mysqli->escape_string($_POST['program']);
 $Tel = $mysqli->escape_string($_POST['Tel']);
 $email = $mysqli->escape_string($_POST['Email']);
-$password = $mysqli->escape_string(password_hash($_POST['IdNo'], PASSWORD_BCRYPT));
+$password = $mysqli->escape_string(password_hash($_POST['AdmNo'], PASSWORD_BCRYPT));
 $hash = $mysqli->escape_string(md5(rand(0, 1000)));
 
 // Check if Admission number and Id Number already exists
