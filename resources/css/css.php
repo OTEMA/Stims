847e85b3747c07cd6ab7cac9ce39b32ea3620848
Tosby <o2jose43@gmail.com>
2017-05-22 21:10:18 +0300

    Added a resources controller file

---
+++ resources/css/css.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="Appearance.css" rel="stylesheet" type="text/css"/>
+<link href="Style.css" rel="stylesheet" type="text/css"/>
+<link href="flexslider.css" rel="stylesheet" type="text/css"/>
+<link href="font-awesome.css" rel="stylesheet" type="text/css"/>
+<link href="lightcase.css" rel="stylesheet" type="text/css"/>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<!--  JavaScript files-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="../js/Copyright.js" type="text/javascript"></script>
+<script src="../js/easing.js" type="text/javascript"></script>
+<script src="../js/index.js" type="text/javascript"></script>
+<script src="../js/jquery.events.touch.js" type="text/javascript"></script>
+<script src="../js/jquery.flexisel.js" type="text/javascript"></script>
+<script src="../js/jquery.flexslider.js" type="text/javascript"></script>
+<script src="../js/lightcase.js" type="text/javascript"></script>
+<script src="../js/move-top.js" type="text/javascript"></script>
