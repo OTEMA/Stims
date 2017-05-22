d0a8c7dae8e772dd759818ad19e9598f70bc7bd2
Tosby <o2jose43@gmail.com>
2017-05-22 21:10:00 +0300

    Added student's program to the list

--- Registrar/studentsFn.php
+++ Registrar/studentsFn.php
@@ -1,46 +1,49 @@
 <?php
-
 require '../Controller/db.php';
-//$result = $mysqli->query("SELECT * FROM students");
-$result=$mysqli->query("select students.*,programs.prog_name from students join programs on students.prog_id=programs.prog_id");
-
-echo "<table border='1' class='table table-striped'>
+$result = $mysqli->query("select students.*,programs.prog_name from students join programs on students.prog_id=programs.prog_id");
+?>
+ <table class="table table-striped">
     <div class='table responsive'>
-        <thead>
-<tr>
-<th>Admission NO</th>
-<th>Date Of Admission</th>
-<th>Program</th>
-<th>Name</th>
-<th>Date Of Birth</th>
-<th>ID NO</th>
-<th>Email</th>
-<th>Tel</th>
-<th>Image</th>
-<th>Update</th>
-<th>Delete</th>
-</tr>
-</thead>
-<tbody>";
+        <thead class="thead-inverse">
+            <tr>
+                <th></th>
+                <th>Admission NO</th>
+                <th>Date Of Admission</th>
+                <th>Program</th>
+                <th>Name</th>
+                <th>Date Of Birth</th>
+                <th>ID NO</th>
+                <th>Email</th>
+                <th>Tel</th>
+                <th>Image</th>
+                <th>Update</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
 
-while ($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td align='center'>" . $row['AdmNo'] . "</td>";
-    echo "<td>" . $row['DOA'] . "</td>";
-    echo "<td>" . $row['prog_name'] . "</td>";
-    echo "<td>" . $row['Lname'] . '&nbsp;' . '&nbsp;' . $row['Mname'] . '&nbsp;' . '&nbsp;' . $row['Fname'] . "</td>";
-    echo "<td>" . $row['DOB'] . "</td>";
-    echo "<td>" . $row['IdNo'] . "</td>";
-    echo "<td>" . $row['Email'] . "</td>";
-    echo "<td>" . $row['Tel'] . "</td>";
-    echo "<td>" . $row['Image'] . "</td>";
-    echo"<td><button class='btn btn-ready'>Update</button></td>";
-    echo"<td><button class='btn btn-danger'>Delete</button></td>";
-    echo "</tr>";
-}
-echo "</tbody>
-</table>";
-
-$mysqli->close();
-
-
+        <?php
+        while ($row = mysqli_fetch_array($result)) {
+            ?>
+            <tbody>
+                <tr>
+                    <td align="center"><input name="checkbox[]" type="checkbox" id="checkbox[]" value="<?php echo $row['AdmNo']; ?>"></td>
+                    <td><?php echo $row['AdmNo']; ?></td>
+                    <td><?php echo $row['DOA']; ?></td>
+                    <td><?php echo $row['prog_name']; ?></td>
+                    <td><?php echo $row['Lname'] . '&nbsp;' . '&nbsp;' . $row['Mname'] . '&nbsp;' . '&nbsp;' . $row['Fname']; ?></td>
+                    <td><?php echo $row['DOB']; ?></td>
+                    <td><?php echo $row['IdNo']; ?></td>
+                    <td><?php echo $row['Email']; ?></td>
+                    <td><?php echo $row['Tel']; ?></td>
+                    <td><?php echo $row['Image']; ?></td>
+                </tr>
+                <?php
+            }
+            $mysqli->close();
+            ?>
+            <tr>
+                <td colspan="3" align="center" bgcolor="#FFFFFF"><input name="delete" type="submit" id="delete" value="Delete" class="btn btn-block btn-danger"></td>
+            </tr>
+        </tbody>
+    </div>
+ </table>
