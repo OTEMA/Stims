7af458fc279ee5811bbdc5c66e39aca679f14ce3
Tosby <o2jose43@gmail.com>
2017-05-23 17:14:30 +0300

    Added an empty page

---
+++ Admin/Programs/Degree.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+This application is property of Otema and is licenced under otema ©2017
+No part of it whatsoever shall be shared or used unless under written permission by Otema
+A breech of this may lead to prosecution.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
