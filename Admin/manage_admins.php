7cdb62065cf8bebc98f751af1baaaac9fba370b6
Tosby <o2jose43@gmail.com>
2017-05-23 17:43:21 +0300

    Begun adding content to the manage admins page

--- Admin/manage_admins.php
+++ Admin/manage_admins.php
@@ -1,13 +1,28 @@
+<?php
+session_start();
+if ($_SESSION['logged_in'] != 1) {
+    $_SESSION['message'] = "You must log in before viewing your profile page!";
+    header("location: ../Controller/error.php");
+} else {
+
+    $first_name = $_SESSION['first_name'];
+    $last_name = $_SESSION['last_name'];
+    $email = $_SESSION['email'];
+    $active = $_SESSION['active'];
+}
+?>
 <!DOCTYPE html>
-<!--
-This application is property of Otema and is licenced under otema ©2017
-No part of it whatsoever shall be shared or used unless under written permission by Otema
-A breech of this may lead to prosecution.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="../resources/css/Appearance.css" rel="stylesheet" type="text/css"/>
+        <link href="../resources/css/Style.css" rel="stylesheet" type="text/css"/>
+        <link href="../resources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="../resources/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+        <script src="../resources/js/Copyright.js" type="text/javascript"></script>
+        <script src="../resources/js/index.js" type="text/javascript"></script>
+        
     </head>
     <body>
         <?php
