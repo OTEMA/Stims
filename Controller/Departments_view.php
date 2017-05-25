9d202f8cc80736c1bb3747c5376da2d0582af5df
Tosby <o2jose43@gmail.com>
2017-05-25 14:28:18 +0300

    finished the department view controller

---
+++ Controller/Departments_view.php
@@ -0,0 +1,36 @@
+<?php
+require 'db.php';
+$result = $mysqli->query("select department.*,schools.School_name from department join schools on department.School_id=schools.School_id");
+?>
+<table width="400" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+        <td>
+            <form name="form1" method="post" action="">
+                <table class="table table-bordered table-striped table-hover table-responsive">
+                    <thead>
+                        <tr class="bg-primary">
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>action</th>
+                        </tr>
+                    </thead>
+                    <?php
+                    $a = 1;
+                    while ($row = mysqli_fetch_array($result)) {
+                        ?>
+                        <tbody>
+                            <tr>
+                                <td align="center"><?php echo $a++; ?></td>
+                                <td><?php echo $row['School_name']; ?></td>
+                                <td><a href='../Controller/deletedp.php?id=<?php echo $row['Department_id']; ?>' class="btn btn-block btn-danger"> delete</a></td>
+                            </tr>
+                            <?php
+                        }
+                        $mysqli->close();
+                        ?>
+                    </tbody>
+                </table>
+            </form>
+        </td>
+    </tr>
+</table>
